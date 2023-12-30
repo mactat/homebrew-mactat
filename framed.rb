0a79badefa54ed0760819aca91cb47a11b410a94
mactat <maciektatarsk@gmail.com>
2023-12-30 09:27:31 +0100

    Brew formula update for framed version v0.0.9

--- framed.rb
+++ framed.rb
@@ -9,7 +9,7 @@ class Framed < Formula
 
   on_macos do
     url "https://github.com/mactat/framed/releases/download/v0.0.9/framed_0.0.9_darwin_all.tar.gz"
-    sha256 "8af833f6eeaaf3d46be1a1e42bb55b5d25d03b979c1b51bf47ea3ead7aad6fcf"
+    sha256 "1caa89934bb2869d2fabc895eb5c6b568f468003ae34308cd5eb9e16b23a1aaf"
 
     def install
       bin.install "framed"
@@ -17,17 +17,17 @@ class Framed < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/mactat/framed/releases/download/v0.0.9/framed_0.0.9_linux_amd64.tar.gz"
-      sha256 "fb673d320ab8f4313806712ec52c760d839c326da4d608746061d4e067aa48b3"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/mactat/framed/releases/download/v0.0.9/framed_0.0.9_linux_arm64.tar.gz"
+      sha256 "04ade00870e8688f7769b7fea3d4a04cfff753f60ebad19985df34941a78fb48"
 
       def install
         bin.install "framed"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mactat/framed/releases/download/v0.0.9/framed_0.0.9_linux_arm64.tar.gz"
-      sha256 "bad09f668bbed2ac1ff390e319c745f7d7155bafa69721c09b87123d9ca026e7"
+    if Hardware::CPU.intel?
+      url "https://github.com/mactat/framed/releases/download/v0.0.9/framed_0.0.9_linux_amd64.tar.gz"
+      sha256 "1139b193ac48dc340bfb4707b231207ec5c72be53c171b1d9579ce8838af9245"
 
       def install
         bin.install "framed"
